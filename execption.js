24d51c02806d6e436fb05cd28d194c3a2ce88b78
ArunaKodigehalliRajashekar <arunakr.xworkz@gmail.com>
2021-12-08 09:38:29 +0530

    regi

---
+++ execption.js
@@ -0,0 +1,38 @@
+function execption(){
+    var user_name=document.getElementById("res").value;
+    var password=document.getElementById("pass").value;
+    if( user_name!==""){
+        if( user_name.length > 5){
+            if(user_name.match(/^[a-z]/)){
+                if(password!==""){
+                    document.getElementById("msg").innerHTML="login succesful";
+                }else{document.getElementById("msg").innerHTML="enter password";}
+            }else{document.getElementById("msg").innerHTML="invalid user_name";}
+        }else{document.getElementById("msg").innerHTML="user_name must greater than 5"}
+    }else{document.getElementById("msg").innerHTML="user_name empty";}
+
+}
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+//                 else if(password===""){
+//               document.getElementById("msg").innerHTML="enter password";
+//             }else{document.getElementById("msg").innerHTML="login succesful";}
+//         }
+//         }else{
+//              document.getElementById("msg").innerHTML="user_name must greater than 5";
+//             }
+       
+//    }else{ document.getElementById("msg").innerHTML="user_name empty";}
+
+// }
